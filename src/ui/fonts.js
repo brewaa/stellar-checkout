0cf07a4df4b2f7f62b656fbda6393ffb8f8df4e8
brewaa <brewaa@gmail.com>
2018-07-13 20:00:10 +1000

    sigh

--- src/ui/fonts.js
+++
@@ -1,8 +0,0 @@
-import fontawesome from '@fortawesome/fontawesome';
-import faChevronCircleLeft from '@fortawesome/fontawesome-free-solid/faChevronCircleLeft';
-import faCopy from '@fortawesome/fontawesome-free-solid/faCopy';
-import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
-
-fontawesome.library.add(faChevronCircleLeft);
-fontawesome.library.add(faCopy);
-fontawesome.library.add(faSpinner);
--- src/utils/string.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-export function replace (text, search, replacement) {
-  return text.replace(new RegExp(search, 'g'), replacement)
-}
-=======
-export function replace(text, search, replacement) {
-    return text.replace(new RegExp(search, 'g'), replacement);
-};
->>>>>>> parent of a4b59fd... no message
