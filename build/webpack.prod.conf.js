1ec74c2feece0da87c4e0979a21c495651f2ad8f
brewaa <brewaa@gmail.com>
2018-07-13 20:44:12 +1000

    demo = https://brewaa.github.io/stellar-checkout/demo/index.html

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -129,7 +129,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new CopyWebpackPlugin([
       {
         from: path.resolve(__dirname, '../dist'),
-        to: '../dist/demo',
+        to: '../docs/demo',
         ignore: ['.*']
       }
     ])
